14d2c46c4cf863406b751b5fd92c8a0903c24fb3
cabrera0laura <lucas56hoseok@gmail.com>
2025-08-12 22:02:31 -0300

    Add funções de display e button

---
+++ ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBindingCommands.ViewModels
+{
+    public class UsuarioViewModel : INotifyPropertyChanged //InterfaceNotify... precisa de using System.ComponentModel;
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged(string propertyName) // quando ocorrere alguma mudança irá mapear a mudança na informação
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));  
+        }
+
+        private string name = string.Empty;  // Comando para geração automatica de Get e Set (ctrl + r + e) --> cria propriedade
+
+        //propriedade
+        public string Name 
+        {
+            get => name;
+            set 
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            } 
+        }
+        
+        public string DisplayName => $"Nome digitado: {Name}";
+    }
+}
---
+++ Views/UsuarioView.xaml.cs
@@ -0,0 +1,13 @@
+using AppBindingCommands.ViewModels;
+
+namespace AppBindingCommands.Views;
+
+public partial class UsuarioView : ContentPage
+{
+	public UsuarioView()
+	{
+		InitializeComponent();
+		BindingContext = new UsuarioViewModel();
+
+    }
+}
